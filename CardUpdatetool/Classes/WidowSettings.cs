444b191f2b5da04368d26136a0a45b57c9d5265e
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2022-02-26 16:01:05 -0500

    Window settings class helper

---
+++ CardUpdatetool/Classes/WidowSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using UnityEngine;
+
+using BepInEx.Configuration;
+
+namespace CardUpdateTool
+{
+    internal static class Window
+    {
+        static public ConfigEntry<int> FontSize;
+        static public ConfigEntry<int> X;
+        static public ConfigEntry<int> Y;
+        static public ConfigEntry<int> Width;
+        static public ConfigEntry<int> Height;
+
+        public static class Defaults
+        {
+            public static int fontSize = Screen.height / 108;
+            public static int x = (int)(Screen.width * 0.33f);
+            public static int y = (int)(Screen.height * 0.09f);
+            public static int width = (int)(Screen.width * 0.225);
+            public static int height = (int)(int)(Screen.height * 0.273);
+        }
+    }
+}
